5f74db36ba71a82cc10489c04002acecb0c4e6e8
MatanRad <MatanRad@users.noreply.github.com>
2017-06-09 09:03:22 +0300

    Created Matbot Discord module

---
+++ DiscordChat.cs
@@ -0,0 +1,25 @@
+﻿using Matbot.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatbotDiscord
+{
+    class DiscordChat : Matbot.Client.Chat
+    {
+        public DiscordChat(ulong id, ChatType type) : base(new DiscordChatId(id), type)
+        {
+        }
+
+        public override bool IsPrivate()
+        {
+            return true;
+        }
+
+        public DiscordChat(Discord.Server Server, ulong id, ChatType type) : base(new DiscordChatId(Server.Id,id), type)
+        {
+        }        
+    }
+}
---
+++ DiscordChatId.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatbotDiscord
+{
+    class DiscordChatId : Matbot.Client.ChatId
+    {
+        public DiscordChatId(ulong id) : base(DiscordClient.DiscordID,id)
+        {
+
+        }
+
+        public DiscordChatId(ulong serverId,ulong id) : base(DiscordClient.DiscordID, id)
+        {
+            this.ServerID = serverId;
+        }
+
+        public ulong ServerID;
+    }
+}
