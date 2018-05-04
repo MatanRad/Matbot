50838a92dc4113a040741e81d19bf8bb8ec7078e
Matan Radomski <MatanRad@users.noreply.github.com>
2018-05-05 00:44:24 +0300

    Added sample speech handler (SpeechEchoHandler)

---
+++ Handlers/SpeechEchoHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Matbot.Client;
+
+namespace Matbot.Handlers
+{
+    public class SpeechEchoHandler : Structure.SpeechHandler
+    {
+        bool enabled = true;
+
+        public override void HandleSpeech(Message m, string text)
+        {
+            m.Reply(text);
+        }
+
+        public override bool IsEnabled()
+        {
+            return enabled;
+        }
+
+        public override void Stop()
+        {
+            enabled = false;
+        }
+    }
+}
